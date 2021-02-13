02ff785ebe9d969a0c1a8ba2a062383f5451f3da
PetrovSergSerg <petrov.serg.serg@gmail.com>
2021-02-13 20:24:19 +0300

    Change behaviour on business layer to entity layer for contacts

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -2,44 +2,25 @@ from data.user import User
 from model.contact import Contact
 
 
-def test_delete_first_contact_from_list(app):
+def test_delete_any_contact_from_list(app):
     app.session.login(User.ADMIN)
     contact1 = Contact(lastname='first', firstname='contact')
     contact2 = Contact(lastname='second', firstname='contact')
     app.contact.create(contact1)
     app.contact.create(contact2)
-    app.contact_list.delete_first_contact()
+    app.contact_list.select_any_contact().delete()
     app.session.logout()
 
 
-def test_delete_last_contact_from_list(app):
+def test_delete_any_contact_from_itself(app):
     app.session.login(User.ADMIN)
     contact1 = Contact(lastname='first', firstname='contact')
     contact2 = Contact(lastname='second', firstname='contact')
     app.contact.create(contact1)
     app.contact.create(contact2)
-    app.contact_list.delete_last_contact()
+    app.contact_list.get_any_contact().delete()
     app.session.logout()
 
-
-def test_delete_first_contact_from_itself(app):
-    app.session.login(User.ADMIN)
-    contact1 = Contact(lastname='first', firstname='contact')
-    contact2 = Contact(lastname='second', firstname='contact')
-    app.contact.create(contact1)
-    app.contact.create(contact2)
-    app.contact_list.get_first_contact().delete()
-    app.session.logout()
-
-
-def test_delete_last_contact_from_itself(app):
-    app.session.login(User.ADMIN)
-    contact1 = Contact(lastname='first', firstname='contact')
-    contact2 = Contact(lastname='second', firstname='contact')
-    app.contact.create(contact1)
-    app.contact.create(contact2)
-    app.contact_list.get_last_contact().delete()
-    app.session.logout()
 
 
 
