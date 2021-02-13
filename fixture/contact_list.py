c1a9bc90831d71ee663c1d893a515a8db8b23b72
PetrovSergSerg <petrov.serg.serg@gmail.com>
2021-02-13 20:24:47 +0300

    forgotten change to commit

--- fixture/contact_list.py
+++ fixture/contact_list.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from random import randint
 
 
 class ContactListHelper:
@@ -9,54 +10,32 @@ class ContactListHelper:
         self.menu = app.menu
         self.contact = app.contact
 
-    def get_first_contact(self):
-        wd = self.app.wd
-        self.menu.home()
-
-        edit = wd.find_element_by_xpath("//img[@title='Edit']")
-        edit.click()
-
-        return self.contact
-
-    def get_last_contact(self):
+    def get_any_contact(self):
         wd = self.app.wd
         self.menu.home()
 
         edit_list = wd.find_elements_by_xpath("//img[@title='Edit']")
         assert len(edit_list) > 0
 
-        edit_last = edit_list[len(edit_list) - 1]
-        edit_last.click()
+        edit = edit_list[randint(0, len(edit_list) - 1)]
+        edit.click()
 
         return self.contact
 
-    def delete_first_contact(self):
-        wd = self.app.wd
-        self.menu.home()
-
-        wd.find_element_by_name("selected[]").click()
-
-        delete = wd.find_element_by_xpath("//input[@type='button'][@value='Delete']")
-        delete.click()
-
-        try:
-            WebDriverWait(wd, 1).until(EC.alert_is_present(), 'Не дождались алёрта')
-            alert = wd.switch_to.alert
-            alert.accept()
-        except TimeoutException:
-            print("no alert")
-        finally:
-            self.menu.home()
-
-    def delete_last_contact(self):
+    def select_any_contact(self):
         wd = self.app.wd
         self.menu.home()
 
         checkbox_list = wd.find_elements_by_name("selected[]")
         assert len(checkbox_list) > 0
 
-        checkbox_last = checkbox_list[len(checkbox_list) - 1]
-        checkbox_last.click()
+        checkbox = checkbox_list[randint(0, len(checkbox_list) - 1)]
+        checkbox.click()
+
+        return self
+
+    def delete(self):
+        wd = self.app.wd
 
         delete = wd.find_element_by_xpath("//input[@type='button'][@value='Delete']")
         delete.click()
